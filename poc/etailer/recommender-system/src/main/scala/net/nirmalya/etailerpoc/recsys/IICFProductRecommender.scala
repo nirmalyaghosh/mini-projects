18f094189b8e678b774a7e7c5a0c8543f501e0e6
Nirmalya Ghosh <nirmalya.ghosh@gmail.com>
2014-10-12 02:06:29 +0800

    Updated item-item recommender

--- poc/etailer/recommender-system/src/main/scala/net/nirmalya/etailerpoc/recsys/IICFProductRecommender.scala
+++ poc/etailer/recommender-system/src/main/scala/net/nirmalya/etailerpoc/recsys/IICFProductRecommender.scala
@@ -1,24 +1,29 @@
 package net.nirmalya.etailerpoc.recsys
 
 import java.io.File
-import java.util.List
-import org.apache.mahout.cf.taste.impl.model.file.FileDataModel
-import org.apache.mahout.cf.taste.impl.recommender.GenericItemBasedRecommender
-import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity
-import org.apache.mahout.cf.taste.recommender.RecommendedItem
+
 import scala.collection.JavaConverters._
 
-class IICFProductRecommender {
+import org.apache.mahout.cf.taste.impl.model.file.FileDataModel
+import org.apache.mahout.cf.taste.impl.recommender.GenericBooleanPrefItemBasedRecommender
+import org.apache.mahout.cf.taste.impl.similarity.TanimotoCoefficientSimilarity
+
+/**
+ * Item-based recommender.
+ * 
+ * @author Nirmalya Ghosh
+ */
+object IICFProductRecommender {
 
   def main(args: Array[String]) {
     val modelFile = args(0)
     val itemId = args(1).toLong
     val howMany = args(2).toInt
     val model = new FileDataModel(new File(modelFile))
-    val itemSimilarity = new PearsonCorrelationSimilarity(model)
-    val recommender = new GenericItemBasedRecommender(model, itemSimilarity)
-    val similarItems = recommender.mostSimilarItems(itemId, howMany).asScala
-    similarItems.foreach(r => println(r.getItemID() + ": " + r.getValue()))
+    val itemSimilarity = new TanimotoCoefficientSimilarity(model)
+    val recommender = new GenericBooleanPrefItemBasedRecommender(model, itemSimilarity)
+    val recommendedItems = recommender.recommend(itemId, howMany).asScala
+    recommendedItems.foreach(r => println(r.getItemID() + ":" + r.getValue()))
   }
 
 }
